454cadb4bca1fd695e10f1a4736d2ef7133af533
barbarosa <elias.barbarosa@gmail.com>
2016-04-24 17:11:31 +0300

    Recycler view with museums

--- app/src/main/java/gr/museum/app/museumapp/utils/RetrofitManager.java
+++ app/src/main/java/gr/museum/app/museumapp/utils/RetrofitManager.java
@@ -22,7 +22,8 @@ import rx.schedulers.Schedulers;
 public class RetrofitManager {
 
 
-    private static final String API_URL = "http://192.168.1.7:8080/";
+    //private static final String API_URL = "http://192.168.1.7:8080/";
+    private static final String API_URL = "http://museum.ddns.net:8080/";
     private static final String TAG = "RetrofitManager";
     private Context context;
     /*private Handler handler;*/
